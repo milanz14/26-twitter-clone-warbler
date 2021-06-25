4e9fd57569dedf1cbc4a6039749c5eb478d01fb2
milanzagorac <zagoracm@gmail.com>
2021-06-24 20:22:52 -0400

    updates/fixes

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -114,7 +114,7 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    session.pop(CURR_USER_KEY)
+    do_logout()
     flash('Successfully logged out!', 'success')
     return redirect('/login')
 
